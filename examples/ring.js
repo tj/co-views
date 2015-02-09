5754b94c6ed7181dceec55a644dd2f6bb6ac0353
Kurten Chan <kurten.chan@yun56.co>
2015-02-09 21:14:07 +0800

    add dust render example

---
+++ examples/ring.js
@@ -0,0 +1,34 @@
+/**
+ * Module dependencies.
+ */
+
+var co = require('co');
+var views = require('..');
+
+var render = views('examples', {
+  ext: 'dust'
+});
+
+var tobi = {
+  name: 'tobi',
+  species: 'ferret'
+};
+
+var loki = {
+  name: 'loki',
+  species: 'ferret'
+};
+
+var luna = {
+  name: 'luna',
+  species: 'cat'
+};
+
+co(function *(){
+  var a = render('ring', { user: tobi });
+  var b = render('user.jade', { user: loki });
+  var c = render('user.ejs', { user: luna });
+  var html = yield [a, b, c];
+  html = html.join('');
+  console.log(html);
+})();
